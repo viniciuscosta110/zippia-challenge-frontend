057aeb39ed51263ce85b4e94604afc4da26e0478
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-11-24 20:49:25 -0300

    Feat: fetch rook

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,9 @@
-import { List, ListItem } from '@mui/material'
-
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { JobCard } from './components/JobCard'
-
-type IPost = {
-  jobTitle: string
-  companyName: string
-  jobdesc: string
-}
+import { Home } from './views/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [jobs, setJobs] = useState<IPost[]>([])
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    axios.post('https://www.zippia.com/api/jobs/', {
-      companySkills: true,
-      dismissedListingHashes: [],
-      fetchJobDesc: true,
-      jobTitle: 'Business Analyst',
-      locations: [],
-      numJobs: 20,
-      previousListingHashes: []
-    }).then((res) => {
-      const cleanJobs = res.data.jobs.map((item: any) => {
-        return {
-          jobTitle: item.jobTitle,
-          companyName: item.companyName,
-          jobdesc: item.jobDescription
-        }
-      })
-
-      setJobs(cleanJobs)
-      setData(res.data.jobs)
-    })
-  }, [])
   return (
     <div className="App">
-      <List sx={{ gap: '20px'}}>
-        {jobs.map((item, index) => {
-          return (
-            <ListItem key={index}>
-              <JobCard companyName={item.companyName} jobTitle={item.jobTitle} description={item.jobdesc} index={index} />    
-            </ListItem>
-          )
-        })}
-      </List>
+      <Home />
     </div>
   )
 }
---
+++ src/api/useFetch.ts
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface useFetchProps {
+  url: string;
+  jobTitle?: string;
+  location?: string;
+}
+
+type IPost = {
+  jobTitle: string
+  companyName: string
+  jobdesc: string
+  daysAgo: number
+}
+
+export function useFetch({ url, jobTitle, location }: useFetchProps) {
+
+  const [data, setData] = useState([]);
+  const [jobs, setJobs] = useState<IPost[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const dateStringToNumber = (date: string) => {
+    //Remove all non-numeric characters
+    const dateNumber = date.replace(/\D/g, "");
+    return parseInt(dateNumber);
+  }
+
+  useEffect(() => {
+    axios.post(url, {
+      companySkills: true,
+      dismissedListingHashes: [],
+      fetchJobDesc: true,
+      jobTitle: 'Business Analyst',
+      locations: [''],
+      numJobs: 20,
+      previousListingHashes: []
+    }).then((response) => {
+      const cleanJobs = response.data.jobs.map((item: any) => {
+        const daysAgo = dateStringToNumber(item.postedDate)
+        return {
+          jobTitle: item.jobTitle,
+          companyName: item.companyName,
+          jobdesc: item.jobDescription,
+          daysAgo
+        }
+      })
+
+      setJobs(cleanJobs)
+      setData(response.data.jobs)
+    }).finally(() => {
+      setLoading(false);
+    })
+    
+  }, [])
+
+  return {data, jobs, loading};
+}
