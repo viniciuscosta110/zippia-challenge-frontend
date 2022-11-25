890d5842726f105b597322ab8ae589d718fffc64
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-11-25 02:19:57 -0300

    Fix: swap variables

--- src/components/JobCard/index.tsx
+++ src/components/JobCard/index.tsx
@@ -23,11 +23,11 @@ export function JobCard({ companyName, jobTitle, description, index }: CardProps
     <Card sx={ cardStyle }>
       <CardContent sx={{ heigth: '100%', display: 'flex', flexDirection: 'column' }}>
         <Typography variant='h1' sx={{ fontSize: '24px' }}>
-          {companyName} 
+          {jobTitle} 
         </Typography >
         
         <Typography variant='h2' sx={{ fontSize: '16px', color: '#777' }}>
-          {jobTitle} 
+          {companyName} 
         </Typography>
 
         <StyledMarkdown style={{ whiteSpace: 'pre-wrap', overflow: 'hidden', fontSize: '16px',  marginTop: '24px', fontFamily: ' "Roboto","Helvetica","Arial",sans-serif ', height: !open ? '200px' : '100%', transition: 'height 1s ease' }}>{ description }</StyledMarkdown>
